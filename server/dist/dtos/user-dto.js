684a966a90a8bef38f654777dbaf5e6d9842347c
s1zykh <vladik.sizykh21@mail.ru>
2024-09-17 23:28:28 +0300

    Remove dist folder from repository

--- server/dist/dtos/user-dto.js
+++
@@ -1,7 +0,0 @@
-export default class UserDTO {
-    constructor(model) {
-        this.email = model.email;
-        this.id = model.id;
-        this.isActivated = model.isActivated;
-    }
-}
--- server/dist/routes/task.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import userController from "../controllers/user.controller.js";
-const router = Router();
-router.post("/registration", userController.registration);
-router.post("/login", userController.login);
-router.get("/users", userController.getUsers);
-export default router;
